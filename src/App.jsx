ecfbf4febb9a09a66e7641b1a680a7eff444fe03
Jovanka <samudrajovanka20@gmail.com>
2022-02-03 23:40:14 +0700

    feat: details page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'assets/scss/style.scss';
 import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage} />
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/properties/:id" component={DetailsPage} />
       </Router>
     </div>
   );
